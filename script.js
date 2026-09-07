d125010d5d84fcd57cdbe323a442f9d2ee0c620a
Belrad Sohounhloue <sodesohounhloue@gmail.com>
2026-09-07 14:13:31 +0000

    Expense tracker 1.3

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-const transactions = [];
+const form = document.getElementById("TransactionForm");
+const type = document.getElementById("transactionType");
+const amount = document.getElementById("amount");
+const nameInput = document.getElementById("name");
 
-function addTransaction(name, amount, type){
-    transactions.push({name, amount, type});
+const balance = document.getElementById("currentBalance");
+const history = document.getElementById("historyList");
 
+let transactions = [];
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    transactions.push({
+        name: nameInput.value,
+        amount: Number(amount.value),
+        type: type.value
+    });
+
+    updateUI();
+    form.reset();
+});
+
+function updateUI(){
+    history.innerHTML = "";
+
+    let income = 0;
+    let expense = 0;
+
+    transactions.forEach(t => {
+        const li = document.createElement("li");
+        li.textContent = `${t.name} : ${t.amount.toFixed(2)} €`;
+        history.appendChild(li);
+
+        if(t.type === "income"){
+            income += t.amount;
+        }else{
+            expense += t.amount;
+        }
+    });
+
+    balance.textContent = (income - expense).toFixed(2) + " €";
 }
-
-
-
-const revenus = transactions
-.filter(t => t.type === "Revenu")
-.reduce((a,b)=>a+b.amount,0);
-
-
-localStorage.setItem("transactions", JSON.stringify(transactions));
-const saved = JSON.parse(localStorage.getItem("transactions"));
